d46efad22f4c0dd770c60d1776c3b6eefbe758a2
Illia0807 <illia.andriienko777@gmail.com>
2025-04-28 11:57:47 +0300

    sd

---
+++ server.js
@@ -0,0 +1,23 @@
+// const express = require("express");
+// const cors = require("cors");
+// const path = require("path");
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+app.use(cors());
+// const PORT = process.env.PORT || 3001;
+
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`running on port ${process.env.PORT || 3001}`);
+});
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./dist", "index.html"));
+});
